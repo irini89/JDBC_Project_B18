1d8a88fa6f8f81db0bdd0e671b142aa41b1c1f93
irini1989 <desara.gjoni@hotmail.com>
2020-08-03 15:46:48 -0400

    getting more info about resultSet

---
+++ src/test/java/com/cybertek/day1/GettingMoreInfoAboutResultSet.java
@@ -0,0 +1,41 @@
+package com.cybertek.day1;
+
+import java.sql.*;
+import java.util.Iterator;
+
+public class GettingMoreInfoAboutResultSet {
+    public static void main(String[] args) throws SQLException {
+        String connectionStr = "jdbc:oracle:thin:@54.90.25.217:1521:XE";
+        String username = "hr";
+        String password = "hr";
+        Connection conn = DriverManager.getConnection(connectionStr, username, password);
+        Statement stmnt = conn.createStatement();
+        ResultSet rs = stmnt.executeQuery("SELECT * FROM EMPLOYEES");
+
+        // resultSet MetaData is data about the resultSet like column count, column name
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        //counting how many columns we have in our resultSet object
+       int columnCount = rsmd.getColumnCount();
+        System.out.println("columnCount = " + columnCount);
+
+        //find out column name according to the index
+        String secondColumnName= rsmd.getColumnName(2);
+        System.out.println("secondColumnName = " + secondColumnName);
+
+        //how to list all the column names from the resultSet
+        // you know how many column we have using getColumnCountName method
+        // you know how to get column name from index getColumnCountName method
+
+        for(int i =1; i <= columnCount; i++){
+           // System.out.println(rsmd.getColumnName(i));
+            System.out.println("Number " + i + " Column name is: " + rsmd.getColumnName(i));
+        }
+
+        // getting column count we need resultSetMetaData object
+        //getting row count
+        //we will use rs.last() to move to last row then call rs.getRow() method
+          // that will be the row count of the entire resultSet
+
+    }
+}
---
+++ src/test/java/com/cybertek/day1/IteratingResultSetBackward.java
@@ -0,0 +1,44 @@
+package com.cybertek.day1;
+
+import java.sql.*;
+
+public class IteratingResultSetBackward {
+    public static void main(String[] args) throws SQLException {
+        String connectionStr = "jdbc:oracle:thin:@54.90.25.217:1521:XE";
+        String username = "hr";
+        String password = "hr";
+        Connection conn = DriverManager.getConnection(connectionStr, username, password);
+        Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
+
+        //first move to the after last location
+        // then move to previous row as long as there  is more  row above
+        rs.afterLast();
+
+      //  rs.previous();
+
+       // System.out.println(rs.getString(1) + " " + rs.getString(2)  );
+
+        while ( rs.previous()==true ){
+            System.out.println(rs.getString(1) + " " + rs.getString(2));
+
+        }
+        System.out.println("------------ more moving-----------------------------------");
+        // how to move to second row from here
+        rs.absolute(2);
+
+        System.out.println("currently i should be at 2nd row" + rs.getRow());
+
+        System.out.println(rs.getString(1) + " " +rs.getString(2));
+
+        rs.first();
+        System.out.println(rs.getString(1) + " " +rs.getString(2));
+
+        rs.last();
+        System.out.println(rs.getString(1) + " " +rs.getString(2));
+
+        // how do i know which number i am right now at this location
+        //get row() method return the row number
+        System.out.println("rs.getRow() = " + rs.getRow());
+    }
+}
